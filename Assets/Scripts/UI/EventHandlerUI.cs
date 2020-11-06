2e8ab5bc9b124d17406da4efd417be4345f31474
Maro5ak <marosak68.2002@gmail.com>
2020-11-06 08:37:28 +0100

    Infopanel UI

---
+++ Assets/Scripts/UI/EventHandlerUI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventHandlerUI : MonoBehaviour{
+    public delegate void ActionChangeHandler(Transform entity, string action);
+    public static event ActionChangeHandler OnActionChange;
+
+    public static void ActionChanged(Transform entity, string action){
+        if(OnActionChange != null) {
+            OnActionChange(entity, action);
+        }
+    }
+
+
+
+
+
+}
---
+++ Assets/Scripts/UI/UIController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour{
+
+    public RectTransform infoPanel;
+
+    private bool infoPanelVisible;
+
+    Animal animal;
+
+    private void Start() {
+        EventHandlerUI.OnActionChange += UpdateInfo;
+
+        infoPanelVisible = false;
+
+        infoPanel.gameObject.SetActive(infoPanelVisible);
+    }
+
+    private void Update() {
+        if(infoPanelVisible){            
+            UpdateNeeds();
+        }
+        Ray interactionRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit interactionInfo;
+        if(Physics.Raycast(interactionRay, out interactionInfo, Mathf.Infinity)){
+            GameObject interactedObject = interactionInfo.collider.gameObject;
+
+            if(interactedObject.tag == "Passive"){
+                if(Input.GetMouseButtonDown(0)){
+                    animal = interactedObject.GetComponent<Animal>();
+                    ToggleInfoPanel();
+                }
+                
+            }
+        }
+    }
+    
+    private void UpdateInfo(Transform entity, string action){
+        Text name = infoPanel.Find("CreatureName").GetComponent<Text>();
+        Text currentAction = infoPanel.Find("CurrentAction").GetComponent<Text>();
+
+        name.text = entity.name;
+        currentAction.text = action;
+    }
+    private void UpdateNeeds(){
+        Slider thirstBar = infoPanel.Find("Thirst").GetComponent<Slider>();
+        Slider hungerBar = infoPanel.Find("Hunger").GetComponent<Slider>();
+        
+        thirstBar.value = animal.thirst;
+        hungerBar.value = animal.hunger;
+
+    }
+   
+
+    private void ToggleInfoPanel(){
+        infoPanelVisible = !infoPanelVisible;
+        infoPanel.gameObject.SetActive(infoPanelVisible);
+    }
+}
